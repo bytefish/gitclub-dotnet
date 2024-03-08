822cc31447206e01a7542539c173c63da9ab1db8
Philipp Wagner <philipp@bytefish.de>
2024-03-08 12:34:54 +0800

    Add Serilog to Startup

--- src/GitClub/Program.cs
+++ src/GitClub/Program.cs
@@ -1,25 +1,67 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog.Filters;
+using Serilog.Sinks.SystemConsole.Themes;
+using Serilog;
 
-// Add services to the container.
+// We will log to %LocalAppData%/RebacExperiments to store the Logs, so it doesn't need to be configured 
+// to a different path, when you run it on your machine.
+string logDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "RebacExperiments");
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// We are writing with RollingFileAppender using a daily rotation, and we want to have the filename as 
+// as "LogRebacExperiments-{Date}.log", the "{Date}" placeholder will be replaced by Serilog itself.
+string logFilePath = Path.Combine(logDirectory, "LogRebacExperiments-.log");
 
-var app = builder.Build();
+// Configure the Serilog Logger. This Serilog Logger will be passed 
+// to the Microsoft.Extensions.Logging LoggingBuilder using the 
+// LoggingBuilder#AddSerilog(...) extension.
+Log.Logger = new LoggerConfiguration()
+    .Filter.ByExcluding(Matching.FromSource("Microsoft.AspNetCore.Diagnostics.ExceptionHandlerMiddleware"))
+    .Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .Enrich.WithEnvironmentName()
+    .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+    .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Configuration
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception exception)
+{
+    Log.Fatal(exception, "An unhandeled exception occured.");
+}
+finally
+{
+    // Wait 0.5 seconds before closing and flushing, to gather the last few logs.
+    await Task.Delay(TimeSpan.FromMilliseconds(500));
+    await Log.CloseAndFlushAsync();
+}
