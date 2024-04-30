4060dc2500e6d4b241a2439150a2ccb6509c2456
Philipp Wagner <philipp@bytefish.de>
2024-04-30 09:11:20 +0800

    Create CurrentUserTests.cs

---
+++ src/GitClub.Tests/Infrastructure/Authentication/CurrentUserTests.cs
@@ -0,0 +1,59 @@
+﻿// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using GitClub.Infrastructure.Authentication;
+using GitClub.Infrastructure.Constants;
+using System.Security.Claims;
+
+namespace GitClub.Tests.Infrastructure.Authentication
+{
+    [TestClass]
+    public class CurrentUserTests
+    {
+        [TestMethod]
+        public void IsInRole_CurrentUserIsInRole()
+        {
+            // Arrange
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Role, Roles.User),
+                new Claim(ClaimTypes.Role, Roles.Administrator)
+            };
+
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(claims));
+
+            var currentUser = new CurrentUser
+            {
+                Principal = claimsPrincipal
+            };
+
+            // Act
+            var isAdministrator = currentUser.IsInRole(Roles.Administrator);
+
+            // Assert
+            Assert.AreEqual(true, isAdministrator);
+        }
+
+        [TestMethod]
+        public void IsInRole_CurrentUserIsNotInRole()
+        {
+            // Arrange
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Role, Roles.User)
+            };
+
+            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(claims));
+
+            var currentUser = new CurrentUser
+            {
+                Principal = claimsPrincipal
+            };
+
+            // Act
+            var isAdministrator = currentUser.IsInRole(Roles.Administrator);
+
+            // Assert
+            Assert.AreEqual(false, isAdministrator);
+        }
+    }
+}
